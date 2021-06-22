f5a109ec0f665a62ef99d722431f71b15492e808
MaulanaAdil <kenari222@gmail.com>
2021-06-23 05:59:36 +0700

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -2,29 +2,47 @@
 require('dotenv').config();
 
 const Hapi = require(`@hapi/hapi`);
+
+// notes
 const notes = require('./api/notes');
 const NotesService = require('./service/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
 
+// users
+const users = require('./api/users');
+const UsersService = require('./service/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+
 const init = async () => {
     const notesService = new NotesService();
+    const usersService = new UsersService();
+
     const server = Hapi.server({
         port: process.env.PORT,
         host: process.env.HOST,
         routes: {
-          cors: {
-              origin: [`*`],
-          },
+            cors: {
+                origin: [`*`],
+            },
         },
     });
 
-    await server.register({
-        plugin: notes,
-        options: {
-            service: notesService,
-            validator: NotesValidator,
+    await server.register([
+        {
+            plugin: notes,
+            options: {
+                service: notesService,
+                validator: NotesValidator,
+            },
         },
-    })
+        {
+            plugin: users,
+            options: {
+                service: usersService,
+                validator: UsersValidator,
+            },
+        },
+    ]);
 
     server.start();
     console.log(`Server running on ${server.info.uri}`)
