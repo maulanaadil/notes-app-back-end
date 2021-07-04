07e76ae5590ff49e61f3b3100c8b11df60bbd9e3
MaulanaAdil <kenari222@gmail.com>
2021-07-04 22:37:12 +0700

    collaborations implemented

--- src/service/postgres/NotesService.js
+++ src/service/postgres/NotesService.js
@@ -6,8 +6,9 @@ const AuthorizationError = require('../../exceptions/AuthorizationError');
 const { mapDBToModel } = require('../../utils');
 
 class NotesService {
-    constructor() {
+    constructor(collaborationsService) {
         this._pool = new Pool();
+        this._collaborationService = collaborationsService;
     }
 
     async addNote({
@@ -33,7 +34,11 @@ class NotesService {
 
     async getNotes(owner) {
         const query = {
-            text: 'SELECT * FROM notes WHERE owner = $1',
+            text: ` SELECT notes.* 
+            FROM notes
+            LEFT JOIN collaborations ON collaborations.note_id = notes.id
+            WHERE notes.owner = $1 OR collaborations.user_id = $1
+            GROUP BY notes.id`,
             values: [owner],
         };
         const result = await this._pool.query(query);
@@ -43,7 +48,10 @@ class NotesService {
 
     async getNoteById(id) {
         const query = {
-            text: 'SELECT * FROM notes WHERE id = $1',
+            text: `SELECT notes.*, users.username 
+            FROM notes 
+            LEFT JOIN users ON users.id = notes.owner
+            WHERE notes.id = $1`,
             values: [id],
         };
         const result = await this._pool.query(query);
@@ -99,6 +107,23 @@ class NotesService {
             throw new AuthorizationError('Anda tidak berhak mengakses resource ini');
         }
     }
+
+    async verifyNoteAccess(noteId, userId) {
+        try {
+            await this.verifyNoteOwner(noteId, userId);
+        } catch (error) {
+            if (error instanceof NotFoundError) {
+                throw error;
+            }
+            try {
+                await this._collaborationService.verifyCollaborator(noteId, userId);
+            } catch {
+                throw error;
+            }
+        }
+    }
+
+
 }
 
 module.exports = NotesService;
